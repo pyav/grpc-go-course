32422036c3c7e535b57c09956d702549f7189780
Anand Verma <verma.anand09@gmail.com>
2021-09-15 20:44:45 +0530

    Unary client and outputs

--- README.md
+++ README.md
@@ -10,3 +10,14 @@ go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 brew install protobuf
 
 protoc greet/greetpb/greet.proto --go_out=plugins=grpc:.
+
+# Unary RPC
+## Output of server run
+Greet Server!
+Greet function was invoked with greeting:{first_name:"Anand" last_name:"Verma"}
+
+## Output of client run
+Hello..I am a client!
+Starting to do a unary RPC...
+2021/09/15 20:38:49 Response from Greet service: Hello Anand
+
--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"../greetpb"
+	"context"
 	"fmt"
 	"google.golang.org/grpc"
 	"log"
@@ -17,5 +18,21 @@ func main() {
 	defer cc.Close()
 
 	c := greetpb.NewGreetServiceClient(cc)
-	fmt.Printf("Created client: %f", c)
+	//fmt.Printf("Created client: %f", c)
+	doUnary(c)
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do a unary RPC...")
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Anand",
+			LastName:  "Verma",
+		},
+	}
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling Greet RPC: %v", err)
+	}
+	log.Printf("Response from Greet service: %v", res.Result)
 }
