e7ffd9e5d8225460839f69f490efc280e1c4a9ce
Anand Verma <anand@Anands-MacBook-Pro.local>
2022-02-23 11:18:57 +0530

    Server streaming and its client

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
+	"io"
 	"log"
 )
 
@@ -19,7 +20,9 @@ func main() {
 
 	c := greetpb.NewGreetServiceClient(cc)
 	//fmt.Printf("Created client: %f", c)
-	doUnary(c)
+	//doUnary(c)
+
+	doServerStreaming(c)
 }
 
 func doUnary(c greetpb.GreetServiceClient) {
@@ -35,4 +38,31 @@ func doUnary(c greetpb.GreetServiceClient) {
 		log.Fatalf("Error while calling Greet RPC: %v", err)
 	}
 	log.Printf("Response from Greet service: %v", res.Result)
+}
+
+func doServerStreaming(c greetpb.GreetServiceClient){
+	fmt.Println("Starting to do a Server streaming RPC...")
+	req := &greetpb.GreetManyTimesRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Anand",
+			LastName:  "Verma",
+		},
+	}
+	resStream, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling GreetManyTimes: %v", err)
+	}
+
+	for {
+		msg, err := resStream.Recv()
+		if err == io.EOF {
+			// we've reached the end of the stream
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error while reading stream: %v", err)
+		}
+		log.Printf("Response from GreetManyTimes: %v", msg.GetResult())
+	}
+
 }
--- greet/greet_server/server.go
+++ greet/greet_server/server.go
@@ -25,13 +25,13 @@ func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.G
 
 func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb.GreetService_GreetManyTimesServer) error {
 	fmt.Printf("GreetManyTimes function was invoked with %v", req)
-	firstName = req.GetGreeting().GetFirstName()
+	firstName := req.GetGreeting().GetFirstName()
 	for i := 0; i < 10; i++ {
 		result := "Hello" + firstName + " number " + strconv.Itoa(i)
 		res := &greetpb.GreetManyTimesResponse {
 			Result: result,
 		}
-		stream.send(res)
+		stream.Send(res)
 		time.Sleep(1000 * time.Millisecond)
 	}
 	return nil
